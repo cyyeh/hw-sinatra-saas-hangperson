36c9ab766b1c24180ae62ba93b775d3d0d6b1199
Chih-Yu Yeh <chihyu.jimmy.yeh@gmail.com>
2018-03-03 08:41:43 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,15 +1,67 @@
 class HangpersonGame
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
-  
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
+  def guess(word)
+    if word == '' || word == nil || /[^a-z]/i.match(word)
+      raise ArgumentError.new('Argument Error')
+    end
+    
+    word.downcase!  
+      
+    if @word.include? word
+      if @guesses.include? word
+        return false
+      else
+        @guesses.concat(word)
+        return true
+      end
+    elsif !@word.include? word
+      if @wrong_guesses.include? word
+        return false
+      else
+        @wrong_guesses.concat(word)
+        return true
+      end
+    end
+  end
+    
+  def word_with_guesses
+    result = ''
+    @word.each_char do |letter|
+      if !@guesses.include? letter
+        result.concat('-')
+      else
+        result.concat(letter)
+      end
+    end
+    
+    return result
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7
+      return :lose
+    elsif @guesses.chars.sort.join == @word.chars.sort.join
+      return :win
+    else
+      return :play
+    end
+  end
+  
+  # helper function: make several guesses
+  def guess_several_letters(letters)
+    letters.chars do |letter|
+      guess(letter)
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
